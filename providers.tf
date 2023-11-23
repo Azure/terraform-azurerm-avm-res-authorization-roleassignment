5d4a1e089ad2a93e03c28bc7dfde49d1990145af
Jared Holgate <jaredholgate@microsoft.com>
2023-11-23 22:20:44 +0000

    Add azuread provider

--- providers.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = ">= 2.46.0"
-    }
-  }
-}
--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,13 @@
 terraform {
   required_version = ">= 1.3.0"
   required_providers {
-    # TODO: Ensure all required providers are listed here.
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.71.0"
+    }
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.46.0"
     }
     random = {
       source  = "hashicorp/random"
