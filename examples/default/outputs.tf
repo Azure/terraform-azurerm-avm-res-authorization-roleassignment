557ec373005156bc43282f724376793e7eff1b5d
Jared Holgate <jaredholgate@microsoft.com>
2023-12-19 13:43:26 +0000

    Fix outputs post linting

--- examples/default/outputs.tf
+++ examples/default/outputs.tf
@@ -1,41 +1,41 @@
 output "users" {
-  value = module.avm-ptn-authorization-roleassignment.users
+  value = module.role_assignments.users
 }
 
 output "groups" {
-  value = module.avm-ptn-authorization-roleassignment.groups
+  value = module.role_assignments.groups
 }
 
 output "app_registrations" {
-  value = module.avm-ptn-authorization-roleassignment.app_registrations
+  value = module.role_assignments.app_registrations
 }
 
 output "system_assigned_managed_identities" {
-  value = module.avm-ptn-authorization-roleassignment.system_assigned_managed_identities
+  value = module.role_assignments.system_assigned_managed_identities
 }
 
 output "user_assigned_managed_identities" {
-  value = module.avm-ptn-authorization-roleassignment.user_assigned_managed_identities
+  value = module.role_assignments.user_assigned_managed_identities
 }
 
 output "all_principals" {
-  value = module.avm-ptn-authorization-roleassignment.all_principals
+  value = module.role_assignments.all_principals
 }
 
 output "role_defintions" {
-  value = module.avm-ptn-authorization-roleassignment.role_defintions
+  value = module.role_assignments.role_defintions
 }
 
 output "role_assignments" {
-  value = module.avm-ptn-authorization-roleassignment.role_assignments
+  value = module.role_assignments.role_assignments
 }
 
 output "entra_id_role_definitions" {
-  value = module.avm-ptn-authorization-roleassignment.entra_id_role_definitions
+  value = module.role_assignments.entra_id_role_definitions
 }
 
 output "entra_id_role_assignments" {
-  value = module.avm-ptn-authorization-roleassignment.entra_id_role_assignments
+  value = module.role_assignments.entra_id_role_assignments
 }
 
 output "test_resource_ids" {
