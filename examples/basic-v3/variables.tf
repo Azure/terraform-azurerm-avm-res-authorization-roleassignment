22b5ccfd7b4f4cc64ec60b0476c5125eca3a36d3
Tobias Lindberg <tobias.ehlert@gmail.com>
2025-06-23 21:43:09 +0200

    chore: remove basic-v3 example

--- examples/basic-v3/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "spn_domain" {
-  type        = string
-  default     = "changeme.com"
-  description = <<DESCRIPTION
-The domain name that is post-fixed on the service principal name.
-This must be a valid domain registered in your Entra ID tenant.
-DESCRIPTION
-}
