5c2d07a406974a76e36c039969a37fd9befd8880
Jared Holgate <jaredholgate@microsoft.com>
2025-07-21 14:02:08 +0100

    fix user object id

--- locals.users.tf
+++ locals.users.tf
@@ -7,19 +7,19 @@ locals {
     local.users_by_object_id
   )
   users_by_employee_id = { for key, value in data.azuread_user.users_by_employee_id :
-    key => value.id
+    key => value.object_id
   }
   users_by_mail = { for key, value in data.azuread_user.users_by_mail :
-    key => value.id
+    key => value.object_id
   }
   users_by_mail_nickname = { for key, value in data.azuread_user.users_by_mail_nickname :
-    key => value.id
+    key => value.object_id
   }
   users_by_object_id = { for key, value in data.azuread_user.users_by_object_id :
-    key => value.id
+    key => value.object_id
   }
   users_by_user_principal_name = { for key, value in data.azuread_user.users_by_user_principal_name :
-    key => value.id
+    key => value.object_id
   }
 }
 
