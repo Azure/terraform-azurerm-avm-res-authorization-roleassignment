8f90a2110e81def1c63f8f81f44e60554793e50e
Puneet V Devadiga <52238436+puneetdevadiga@users.noreply.github.com>
2026-07-13 16:18:05 +0800

    Fix/import app registration foreach (#133)
    * fix: use static app registration keys for import
    
    * chore: run avm pre-commit

--- locals.app.registrations.tf
+++ locals.app.registrations.tf
@@ -3,14 +3,14 @@ locals {
     local.service_principal_by_client_id,
     local.service_principal_by_object_id
   )
-  app_registrations_by_client_id_client_id = { for key, value in data.azuread_application.applications_by_client_id :
-    key => value.client_id
+  app_registrations_by_client_id_client_id = { for key, value in var.app_registrations_by_client_id :
+    key => value
   }
-  app_registrations_by_display_name_client_id = { for key, value in data.azuread_application.applications_by_display_name :
-    key => value.client_id
+  app_registrations_by_display_name_client_id = { for key, value in var.app_registrations_by_display_name :
+    key => data.azuread_application.applications_by_display_name[key].client_id
   }
-  app_registrations_by_object_id_client_id = { for key, value in data.azuread_application.applications_by_object_id :
-    key => value.client_id
+  app_registrations_by_object_id_client_id = { for key, value in var.app_registrations_by_object_id :
+    key => data.azuread_application.applications_by_object_id[key].client_id
   }
   app_registrations_client_ids = merge(
     local.app_registrations_by_display_name_client_id,
@@ -29,12 +29,6 @@ data "azuread_application" "applications_by_display_name" {
   for_each = var.app_registrations_by_display_name
 
   display_name = each.value
-}
-
-data "azuread_application" "applications_by_client_id" {
-  for_each = var.app_registrations_by_client_id
-
-  client_id = each.value
 }
 
 data "azuread_application" "applications_by_object_id" {
